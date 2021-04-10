76216bc8a26fc099b5ba21270240c522c729da98
M Akif Eyler <ma.eyler@gmail.com>
2021-04-10 12:01:24 +0300

    add css & js

---
+++ rama/init.js
@@ -0,0 +1,14 @@
+function goBack() {
+    location.pathname = "/Kitap/rama/"
+}
+function init() {
+    for (let a of document.querySelectorAll('a')) {
+      if (!a.name) continue
+      a.href = "/Kuran/reader.html#"+a.name
+      a.target = "iqra"
+    }
+    for (let b of document.querySelectorAll('.goBack'))
+      b.onclick = goBack
+}
+init()
+  
