c2a595ec766c0a10365e65b7ed31b5104c1d2016
Akif Eyler <ma.eyler@gmail.com>
2023-03-21 09:40:28 +0300

    modify init.js

--- esma/init.js
+++ esma/init.js
@@ -1,5 +1,5 @@
 function goBack() {
-    location.pathname = "/Kitap/rama/"
+    location.pathname = "/Kitap/esma/"
 }
 function init() {
     for (let a of document.querySelectorAll('a')) {
@@ -14,21 +14,20 @@ function init() {
 }
 
 const LINKS = document.createElement('div')
-LINKS.innerHTML = `<br><h3>Paylaşalım lutfen:</h3>
-<p>
-<a href="https://groups.google.com/g/okuyun5"  target="Kuran"><img alt="https://groups.google.com/g/okuyun5" src="/Kitap/icon/email.png" style="width: 36px" /></a>
+LINKS.innerHTML = `<br><b>Paylaşalım lutfen:</b>&ensp;
+<a href="https://groups.google.com/g/okuyun5/about"  target="Kuran"><img alt="https://groups.google.com/g/okuyun5" src="/Kitap/icon/email.png" style="width: 36px" /></a>
 &ensp;
 <a href="https://twitter.com/okuyun5"  target="Kuran"><img alt="https://twitter.com/okuyun5" src="/Kitap/icon/twitter.png" style="width: 36px" /></a>
-&ensp;
+<!-- &ensp;
 <a href="https://instagram.com/okuyun.5/"  target="Kuran"><img alt="https://instagram.com/okuyun.5/" src="/Kitap/icon/instagram.png" style="width: 34px"/></a>
 &ensp;
 <a href="https://linkin.bio/okuyun-5"  target="Kuran"><img alt="https://linkin.bio/okuyun-5" src="/Kitap/icon/link2.png" style="width: 38px" /></a>
-<!-- &ensp; <a href="https://facebook.com/okuyun5"  target="Kuran"><img alt="https://facebook.com/okuyun5" src="/Kitap/icon/facebook.png" style="width: 36px" /></a> -->
+&ensp; <a href="https://facebook.com/okuyun5"  target="Kuran"><img alt="https://facebook.com/okuyun5" src="/Kitap/icon/facebook.png" style="width: 36px" /></a>
 &ensp;
-<a href="https://youtube.com/playlist?list=PLBDiqooDrruv0MDNyRbG-JBLH2CnfNxAd"  target="Kuran"><img alt="https://youtube.com/" src="/Kitap/icon/youtube.png" style="width: 38px" /></a>
+<a href="https://youtube.com/playlist?list=PLBDiqooDrruv0MDNyRbG-JBLH2CnfNxAd"  target="Kuran"><img alt="https://youtube.com/" src="/Kitap/icon/youtube.png" style="width: 38px" /></a> -->
 &ensp;
 <a href="https://github.com/Okuyun/"  target="Kuran"><img alt="https://github.com/Okuyun/" src="/Kitap/icon/github.webp" style="width: 40px" /></a>
-</p>`
+`
 
 init()
   
