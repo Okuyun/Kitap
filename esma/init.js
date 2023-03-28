482d0b15af253986d594e49cc3ab6c3ee4299b80
Akif <ma.eyler@gmail.com>
2023-03-28 23:37:58 +0300

    add goNext button

--- esma/init.js
+++ esma/init.js
@@ -1,20 +1,42 @@
+function getIndex() {
+    let s = location.pathname
+    let i = s.lastIndexOf('/')
+    return NAMES.indexOf(s.slice(i+1))
+}
+function goNext() {
+    let n = getIndex()
+    if (n<0 || n>=14) goBack();
+    else location.pathname = "/Kitap/esma/"+NAMES[n+1]
+}
 function goBack() {
     location.pathname = "/Kitap/esma/"
 }
+function setTitle() {
+    let h3 = document.body.querySelector('h3')
+    // console.log(h3, h3.textContent)
+    if (!h3) return
+    let s = h3.textContent
+    parent.document.title = s.slice(0, s.length-3)
+}
 function init() {
-    for (let a of document.querySelectorAll('a')) {
-      if (!a.name) continue
+    for (let a of document.querySelectorAll('a[name]')) {
+      // if (!a.name) continue
       a.href = "/Kuran/reader.html#"+a.name
       a.target = "iqra"
     }
+    let div = document.createElement('div')
+    div.innerHTML = LINKS
+    document.body.append(div)
     for (let b of document.querySelectorAll('.goBack'))
       b.onclick = goBack
-    document.body.append(LINKS)
-    if (window.title) title.innerText = document.title
+    for (let b of document.querySelectorAll('.goNext'))
+      b.onclick = goNext
+    // if (window.title) title.innerText = document.title
+    setTimeout(setTitle, 50)
 }
+onload = init
 
-const LINKS = document.createElement('div')
-LINKS.innerHTML = `<br><b>Paylaşalım lutfen:</b>&ensp;
+const LINKS = `<br><b>Paylaşalım lutfen:</b>&ensp;
 <a href="https://groups.google.com/g/okuyun5/about"  target="Kuran"><img alt="https://groups.google.com/g/okuyun5" src="/Kitap/icon/email.png" style="width: 36px" /></a>
 &ensp;
 <a href="https://twitter.com/okuyun5"  target="Kuran"><img alt="https://twitter.com/okuyun5" src="/Kitap/icon/twitter.png" style="width: 36px" /></a>
@@ -27,7 +49,37 @@ LINKS.innerHTML = `<br><b>Paylaşalım lutfen:</b>&ensp;
 <a href="https://youtube.com/playlist?list=PLBDiqooDrruv0MDNyRbG-JBLH2CnfNxAd"  target="Kuran"><img alt="https://youtube.com/" src="/Kitap/icon/youtube.png" style="width: 38px" /></a> -->
 &ensp;
 <a href="https://github.com/Okuyun/"  target="Kuran"><img alt="https://github.com/Okuyun/" src="/Kitap/icon/github.webp" style="width: 40px" /></a>
-`
-
-init()
+&emsp;
+<button class=goNext>▶</button>
+`;
+const NAMES = `Allah.html
+Rahman.html
+Malik.html
+Selam.html
+Mumin.html
+Cebbar.html
+Hallak.html
+Bari.html
+Aziz.html
+Alim.html
+Allam.html
+Aliyy.html
+Gafur.html
+Tevvab.html
+Afuv.html
+Ehad.html
+Vahid.html
+Vehhab.html
+Semi.html
+Latif.html
+Vedud.html
+Muhyi.html
+Hayy.html
+Evvel.html
+Zahir.html
+Kadir.html
+Nur.html
+Kebir.html
+Gani.html
+Celal.html`.split('\n')
   
