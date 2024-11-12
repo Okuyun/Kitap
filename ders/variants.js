7245f4543f1054e457a2cfe1a55c0b1dbc11722c
Akif Eyler <ma.eyler@gmail.com>
2024-11-12 12:28:40 +0300

    better structure
    in variants.txt

--- ders/variants.js
+++ ders/variants.js
@@ -2,27 +2,27 @@
 function makeTable() {
   function addVariant(x) {
       if (filter(x)) return
-      let {cv, num, rdr, word, rgn, rasm, std} = x
+      let {cv, num, std, rdr, word, rgn, rasm} = x
       let [c, v] = cv.split(':')
-      let color = (rgn == '*')? 'class=yellow' : ''
+      let color = rgn? (rgn=='*'? 'yellow' : 'pink') : ''
+      let cls = color? 'class='+color : ''
       if (rdr.length > 7) rdr = rdr.replaceAll(' ','')
       let url = 
 "https://corpuscoranicum.de/en/verse-navigator/sura/"+c+"/verse/"+v+"/variants"
       a.push(`<td>${++i}
         <td><a target=iqra href="/Kuran/reader.html#v=${cv}">${cv}</a>
-        <td ${color}>${std}<td>${rdr}
+        <td ${cls}>${std}<td>${rdr}
         <td><a target=Kuran href="${url}">${word}</a>\n`)
   }
     let a = [], i = 0
-    let kk = Object.getOwnPropertyNames(VD._data)
-    kk.pop() // remove length (last key in kk)
-    for (let k of kk) {
+    for (let k of VD._keys) {
       let d = VD.variants(k)
       if (d) d.forEach(addVariant)
     }
-    console.log(i+" entries in the table")
+    console.log(i+" entries in ", VD._keys.length+" verses")
     tablo1.innerHTML 
     = `<tr><th>No<th>Ayet<th>Standart<th>Kıraat<th>Varyant<tr>`
       + a.join('\n<tr>')
+    if (callLater) callLater()
 }
 let VD  = new VariantData('/Kuran/data/variants.txt', makeTable)
